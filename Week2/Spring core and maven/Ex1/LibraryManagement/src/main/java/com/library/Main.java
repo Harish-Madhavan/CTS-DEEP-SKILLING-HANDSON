2263095793f50683895eeac37eab6ae3af0fd695
Harish-Madhavan <veeharish2005@gmail.com>
2026-07-02 17:33:49 +0530

    week2

---
+++ Week2/Spring core and maven/Ex1/LibraryManagement/src/main/java/com/library/Main.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+import com.library.repository.BookRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.printServiceInfo();
+        
+        BookRepository bookRepository = context.getBean("bookRepository", BookRepository.class);
+        bookRepository.printRepositoryInfo();
+    }
+}
---
+++ Week2/Spring core and maven/Ex1/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,9 @@
+package com.library.service;
+
+
+public class BookService {
+
+    public void printServiceInfo() {
+        System.out.println("BookService is active.");
+    }
+}
